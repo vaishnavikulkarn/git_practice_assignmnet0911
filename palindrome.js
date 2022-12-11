10df403c9978798829ec49efcc90d4d8852ef20e
vaishnavi kulkarni <vaishnavikulkarni124@gmailcom>
2022-12-11 12:10:54 +0530

    Adding palindrome program

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+
+let str="naman"
+let bag ="";
+    for ( let i = str.length-1 ; i>=1 ; i--){
+        bag += str[i]
+    }
+    if (bag == str[i]){
+        console.log("Yes")
+    }else {
+        console.log("No")
+    }
+  
